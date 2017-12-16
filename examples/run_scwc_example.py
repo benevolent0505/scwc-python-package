d26a5b1ca350251b3f8b8ba55e5ddf27c794a5ec
benevolent0505 <mikio.fjt@gmail.com>
2017-12-16 14:46:28 +0900

    add sparse matrix example

--- examples/run_scwc_example.py
+++ examples/run_scwc_example.py
@@ -3,14 +3,21 @@
 from scwc import SCWC
 
 from sklearn.datasets import load_wine
+from scipy.sparse import lil_matrix
 
 
 wine = load_wine()
 X = wine.data
 y = wine.target
-header = wine.feature_names
 
 selector = SCWC(verbose=10)
-selector.fit(X, y, header=header)
+selector.fit(X, y)
 
 selected_feature = selector.transform()
+
+
+# sparse
+X = lil_matrix(X)
+
+selector = SCWC(sort='icr', verbose=1)
+selector.fit_transform(X, y)
